4521fd50e91aa43a4ad68c1de051862aa532c30b
maatheusantanadev <mssantana@ecomp.uefs.br>
2024-12-15 20:16:38 -0300

    Atualização do arquivo de cadastro de animais

--- modules.h/animais.h
+++ modules.h/animais.h
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // Estrutura para representar um animal
 typedef struct Animal {
-    int idAnimal;
+    char idAnimal[15];
     char nomeAnimal[50];
-    int idadeAnimal;
+    char idadeAnimal[15];
     char especie[30];
     char cidadeAnimal[50];
     char condicoesAdocao[100];
@@ -15,15 +16,15 @@ typedef struct Animal {
 } Animal;
 
 // Função para criar um novo animal
-Animal* criarAnimalAdocao(int id, char nome[], int idade, char especie[], char cidade[], char condicoes[]) {
+Animal* criarAnimalAdocao(char id[], char nome[], char idade[], char especie[], char cidade[], char condicoes[]) {
     Animal* novo = (Animal*)malloc(sizeof(Animal));
     if (novo == NULL) {
         printf("Erro ao alocar memoria!\n");
         exit(1);
     }
-    novo->idAnimal = id;
+    strcpy(novo->idAnimal, id);
     strcpy(novo->nomeAnimal, nome);
-    novo->idadeAnimal = idade;
+    strcpy(novo->idadeAnimal, idade);
     strcpy(novo->especie, especie);
     strcpy(novo->cidadeAnimal, cidade);
     strcpy(novo->condicoesAdocao, condicoes);
@@ -40,12 +41,12 @@ void salvarAnimal(Animal* animal) {
         return;
     }
 
-    fprintf(arquivo, "ID: %d\n", animal->idAnimal);
+    fprintf(arquivo, "ID: %s\n", animal->idAnimal);
     fprintf(arquivo, "Nome: %s\n", animal->nomeAnimal);
-    fprintf(arquivo, "Idade: %d\n", animal->idadeAnimal);
-    fprintf(arquivo, "Especie: %s\n", animal->especie);
+    fprintf(arquivo, "Idade: %s\n", animal->idadeAnimal);
+    fprintf(arquivo, "Espécie: %s\n", animal->especie);
     fprintf(arquivo, "Cidade: %s\n", animal->cidadeAnimal);
-    fprintf(arquivo, "Condicoes de Adocao: %s\n", animal->condicoesAdocao);
+    fprintf(arquivo, "Condições de Adoção: %s\n", animal->condicoesAdocao);
     fprintf(arquivo, "-------------------------\n");
 
     fclose(arquivo);
@@ -54,47 +55,80 @@ void salvarAnimal(Animal* animal) {
 
 // Função para cadastrar um novo animal na lista encadeada
 void cadastrarAnimal(Animal** inicio, Animal** fim) {
-    int id, idade;
-    char nome[50], especie[30], cidade[50], condicoes[100];
+    char idade[15], id[15], nome[50], especie[30], cidade[50], condicoes[100];
 
     printf("\033[1;32m\nCadastro de Novo Animal:\n\n\033[0m");
 
-    printf("\033[1;34mID do Animal: ");
-    scanf("%d", &id);
-    getchar();
+    // Leitura do ID
+     while (1) {
+        printf("\033[1;34mId do Animal: \033[0m");
+        fflush(stdin);
+        fgets(id, sizeof(id), stdin);
+        id[strcspn(id, "\n")] = '\0';
 
-    printf("Nome do Animal: ");
+        if (strlen(id) == 0) {
+            printf("\033[1;31m\n\nA idade nao pode estar vazia.\n\033[0m");
+        } else {
+            int valido = 1;
+            for (size_t i = 0; i < strlen(id); i++) {
+                if (!isdigit((unsigned char)id[i])) {
+                    valido = 0;
+                    break;
+                }
+            }
+            if (valido) break;
+            printf("\033[1;31m\n\nA id deve conter apenas digitos numericos.\n\033[0m");
+        }
+    }
+
+    // Leitura do Nome
+    printf("\033[1;34mNome do Animal: \033[0m");
     fgets(nome, sizeof(nome), stdin);
     nome[strcspn(nome, "\n")] = '\0';
 
-    printf("Idade do Animal: ");
-    scanf("%d", &idade);
-    getchar();
+    // Leitura da Idade
+    while (1) {
+        printf("\033[1;34mIdade do Animal: \033[0m");
+        fgets(idade, sizeof(idade), stdin);
+        idade[strcspn(idade, "\n")] = '\0';
 
-    printf("Especie do Animal: ");
+        if (strlen(idade) == 0) {
+            printf("\033[1;31m\n\nA idade nao pode estar vazia.\n\033[0m");
+        }else{
+            break;
+        }
+    }
+
+    // Leitura da Espécie
+    printf("\033[1;34mEspecie do Animal: \033[0m");
     fgets(especie, sizeof(especie), stdin);
     especie[strcspn(especie, "\n")] = '\0';
 
-    printf("Cidade do Animal: ");
+    // Leitura da Cidade
+    printf("\033[1;34mCidade do Animal: \033[0m");
     fgets(cidade, sizeof(cidade), stdin);
     cidade[strcspn(cidade, "\n")] = '\0';
 
-    printf("Condicoes de Adocao: ");
+    // Leitura das Condições
+    printf("\033[1;34mCondicoes de Adocao: \033[0m");
     fgets(condicoes, sizeof(condicoes), stdin);
     condicoes[strcspn(condicoes, "\n")] = '\0';
 
+    // Criação do novo animal
     Animal* novo = criarAnimalAdocao(id, nome, idade, especie, cidade, condicoes);
 
-    if (*inicio == NULL) { 
+    // Inserção na lista encadeada
+    if (*inicio == NULL) {
         *inicio = novo;
         *fim = novo;
-    } else { 
+    } else {
         (*fim)->prox = novo;
         novo->ant = *fim;
         *fim = novo;
     }
 
-    salvarAnimal(novo); 
+    // Salvando o animal
+    salvarAnimal(novo);
 }
 
 // Função para ler os dados dos animais do arquivo
@@ -116,3 +150,4 @@ void lerAnimais() {
     fclose(arquivo);
     printf("----------------------------------------------------\n");
 }
+
